213a40129b51223cfcc0e79cb6f4714ab74e9c2d
melchizedek2991 <ayodeleayomidotun@gmail.com>
2023-04-04 11:42:45 -0700

    update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,21 +1,23 @@
 #include "lists.h"
 
 /**
- * sum_listint - Computes the sum of all the integers in a linked list.
+ * sum_listint - Computes the sum of all integers in a linked list.
  *
- * @head: A pointer to the head node of the linked list.
+ * @head: Pointer to the head node of the linked list.
  *
- * Return: The sum of all the integers in the linked list.
+ * Return: The sum of all integers in the linked list.
  */
+
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
+	int sum;
 
-    while (head != NULL)
-    {
-        sum += head->n;
-        head = head->next;
-    }
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-    return sum;
+	return (sum);
 }
