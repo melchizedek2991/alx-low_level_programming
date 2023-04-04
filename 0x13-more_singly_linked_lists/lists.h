796e6ee00430c90590e9650a42556c206d97f67a
melchizedek2991 <ayodeleayomidotun@gmail.com>
2023-04-04 12:24:43 -0700

    updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,7 +10,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for ALX project
  */
 typedef struct listint_s
 {
@@ -18,13 +18,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
 typedef struct listp_s
 {
 	void *p;
