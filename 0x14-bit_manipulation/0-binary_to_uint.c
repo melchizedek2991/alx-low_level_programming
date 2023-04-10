28d65d1fc5def7ab7a50f33732d0119c260a434a
melchizedek2991 <ayodeleayomidotun@gmail.com>
2023-04-10 01:24:16 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0;
+	unsigned int decVal = 0;
 
 	if (!b)
 		return (0);
@@ -18,8 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		decVal = 2 * decVal + (b[i] - '0');
 	}
 
-	return (dec_val);
+	return (decVal);
 }
