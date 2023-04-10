156f75a4853720e3a8ef81e98c7df94417a8d20a
melchizedek2991 <ayodeleayomidotun@gmail.com>
2023-04-09 22:33:37 -0700

    updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int op, wd, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +22,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	op = open(filename, O_WRONLY | O_APPEND);
+	wd = write(o, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (op == -1 || wd == -1)
 		return (-1);
 
-	close(o);
+	close(op);
 
 	return (1);
 }
