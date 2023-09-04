2c6abb5ddb414bf755537da33a919f52553f915a
Ayodele Ayomidotun <ayodeleayomidotun@gmail.com>
2023-09-04 13:23:00 -0700

    All Tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,36 +3,36 @@
 /**
  * create_file - creates a file
  * @filename: filename.
- * @text_content: The content to writed in the file.
+ * @text_content: content writed in the file.
  *
  * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d;
-	int n_letters;
-	int bytwr;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file_d == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (n_letters = 0; text_content[n_letters]; n_letters++)
+	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	bytwr = write(fd, text_content, n_letters);
+	rwr = write(fd, text_content, nletters);
 
-	if (bytwr == -1)
+	if (rwr == -1)
 		return (-1);
 
-	close(file_d);
+	close(fd);
 
 	return (1);
 }
